a6fa9e12da3e2fa4e797e29e07f369d5dc351e81
Bruno Luiz Purcinelli <bruno.purcinelli@outlook.com.br>
2020-08-19 16:59:56 -0300

    Update config.php

--- src/web/payment/php/config.php
+++ src/web/payment/php/config.php
@@ -5,15 +5,15 @@ date_default_timezone_set($timezone);
 $sandBox = 1;
 
 if ($sandBox == 0) {
-    $emailPagseguro = "purcinelli_k19@hotmail.com";
-    $tokenPagseguro = "e615dfc7-7c74-409b-a271-4265c6831a0b363db03d4f7ba9e5cbae0441d3fc5d4f346f-43e3-45b0-8f6f-23d0759a76d7";
+    $emailPagseguro = "Email_PagSeguro";
+    $tokenPagseguro = "Token_PagSeguro";
 
     $scriptPagseguro = "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
     $urlPagseguro = "https://ws.pagseguro.uol.com.br/v2/";
 
 } else {
-    $emailPagseguro = "purcinelli_k19@hotmail.com";
-    $tokenPagseguro = "5E53502523F6411AA4A544FE794F4C82";
+    $emailPagseguro = "Email_SandBox_PagSeguro";
+    $tokenPagseguro = "Token_SandBox_PagSeguro";
 
     $scriptPagseguro = "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
     $urlPagseguro = "https://ws.sandbox.pagseguro.uol.com.br/v2/";
